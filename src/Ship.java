e72637ae4f7576f0fcb24e950bedd4020baf7644
YashedP <yashjani144@gmail.com>
2023-02-21 21:18:15 -0500

    Added ship.java

---
+++ src/Ship.java
@@ -0,0 +1,78 @@
+public class Ship
+{
+    private static final int UNSET = -1;
+    private static final int HORIZONTAL = 0;
+    private static final int VERTICAL = 1;
+    
+    private int row;
+    private int col;
+    private int length;
+    private int direction;
+    
+    public Ship(int length) {
+        this.length = length;
+        this.row = UNSET;
+        this.col = UNSET;
+        this.direction = UNSET;
+    }
+    
+    public boolean isLocationSet() {
+        if(row == UNSET || col == UNSET) {
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean isDirectionSet() {
+        if(direction == UNSET) {
+            return false;
+        }
+        return false;
+    }
+    
+    public void setLocation(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+    
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    
+    public int getCol() {
+        return col;
+    }
+    
+    public int getLength() {
+        return length;
+    }
+    
+    public int getDirection() {
+        return direction;
+    }
+    
+    private String directionToString() {
+        if(direction == 0) {
+            return "horizontal";
+        }
+        else if(direction == 1) {
+            return "vertical";
+        }
+        return "unset direction";
+    }
+    
+    private String locationToString() {
+        if(isLocationSet()) {
+            return row + ", " + col;
+        }
+        return "unset location";
+    }
+    
+    public String toString() {
+        return directionToString() + " ship of length " + getLength() + " at (" + locationToString() + ")";
+    }
+}
